844cf0276a3a27c074274c6fceaf568db26287b8
BerkayOzturk <berkayozturkce@gmail.com>
2023-01-15 20:46:33 +0300

    ListNum Updated

--- csharpTrain/TelefonRehberi/ListNum.cs
+++ csharpTrain/TelefonRehberi/ListNum.cs
@@ -4,20 +4,46 @@ namespace TelRehberi
     {
         public static void ListNumber(List<Contact> myContact)
         {
-            Console.WriteLine("Telefon Rehberi");
-            Console.WriteLine("**********************************************");
-            foreach (var contact in myContact)
+
+            while (true)
             {
-                Console.WriteLine($"İsim: {contact.Name}");
-                Console.WriteLine($"Soyisim: {contact.Surname}");
-                Console.WriteLine($"Telefon Numarası: {contact.Phone}");
-                Console.WriteLine("-");
+                List<Contact> newContact = new List<Contact>();
+
+                Console.Clear();
+
+                Console.Write("(1) A-Z\n(2) Z-A\nseciminiz: ");
+                char input = Convert.ToChar(Console.ReadLine());
+                switch (input)
+                {
+                    case '1':
+                        newContact = myContact.OrderBy(x => x.Name).ToList();
+                        break;
+                    case '2':
+                        newContact = myContact.OrderByDescending(x => x.Name).ToList();
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+                Console.Clear();
+                Console.WriteLine("Telefon Rehberi");
+                Console.WriteLine("**********************************************");
+                foreach (var contact in newContact)
+                {
+                    Console.WriteLine($"İsim: {contact.Name}");
+                    Console.WriteLine($"Soyisim: {contact.Surname}");
+                    Console.WriteLine($"Telefon Numarası: {contact.Phone}");
+                    Console.WriteLine("-");
+                }
+                Console.ReadKey();
+                return;
             }
-            Console.ReadKey();
 
         }
 
-        
+
 
     }
 }
